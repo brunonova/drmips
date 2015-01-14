7a6e45e0a832c3ec7fcda63a00de47390864e470
Bruno Nova <4999472+brunonova@users.noreply.github.com>
2015-01-14 14:41:58 +0000

    PC: add DrMIPS homepage to "About" dialog

--- src/simulator/DrMIPSSimulator/src/org/feup/brunonova/drmips/simulator/AppInfo.java
+++ src/simulator/DrMIPSSimulator/src/org/feup/brunonova/drmips/simulator/AppInfo.java
@@ -31,6 +31,8 @@ public class AppInfo {
 	public static final String DESCRIPTION = "Educational MIPS simulator";
 	/** Version of the application. */
 	public static final String VERSION = "1.2.3";
+	/** Homepage of the application. */
+	public static final String HOMEPAGE = "https://bitbucket.org/brunonova/drmips";
 	/** Name of the main author (creator) of the application. */
 	public static final String MAIN_AUTHOR_NAME = "Bruno Nova";
 	/** E-mail address of the main author (creator) of the application. */
