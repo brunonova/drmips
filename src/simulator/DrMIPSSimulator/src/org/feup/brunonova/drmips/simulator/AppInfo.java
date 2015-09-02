12f327d687be1bbf31229827b5ba48445d55d22e
Bruno Nova <4999472+brunonova@users.noreply.github.com>
2015-09-02 13:38:37 +0100

    Bump version to 1.3.1

--- src/simulator/DrMIPSSimulator/src/org/feup/brunonova/drmips/simulator/AppInfo.java
+++ src/simulator/DrMIPSSimulator/src/org/feup/brunonova/drmips/simulator/AppInfo.java
@@ -30,7 +30,7 @@ public class AppInfo {
 	/** Short description of the application. */
 	public static final String DESCRIPTION = "Educational MIPS simulator";
 	/** Version of the application. */
-	public static final String VERSION = "1.3.0";
+	public static final String VERSION = "1.3.1";
 	/** Homepage of the application. */
 	public static final String HOMEPAGE = "https://bitbucket.org/brunonova/drmips";
 	/** Name of the main author (creator) of the application. */
