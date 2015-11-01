106228e14d96d28fe55f8ef3df5dce5984f76e80
Bruno Nova <4999472+brunonova@users.noreply.github.com>
2015-11-01 11:44:41 +0000

    Add "test" target to CMake

---
+++ src/simulator/DrMIPSSimulator/test/org/feup/brunonova/drmips/simulator/mips/components/TestSuite.java
@@ -0,0 +1,34 @@
+/*
+    DrMIPS - Educational MIPS simulator
+    Copyright (C) 2013-2015 Bruno Nova <brunomb.nova@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.feup.brunonova.drmips.simulator.mips.components;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({SignExtendTest.class,
+                     NotTest.class,
+					 XorTest.class,
+					 ShiftLeftTest.class,
+					 ZeroExtendTest.class,
+					 ConcatenatorTest.class,
+					 ForkTest.class})
+public class TestSuite {
+	
+}
